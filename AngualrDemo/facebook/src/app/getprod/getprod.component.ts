b9559f8fc156c88e34cd29e8dc6a7ce87160a217
Thaneshwara848 <thaneshwaramanjunatha@gmail.com>
2022-09-05 22:11:25 +0530

    proj Code

--- AngualrDemo/facebook/src/app/getprod/getprod.component.ts
+++ AngualrDemo/facebook/src/app/getprod/getprod.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonservService } from '../commonserv.service';
 import { GetmyprodservService } from '../getmyprodserv.service';
 
 @Component({
@@ -9,6 +10,46 @@ import { GetmyprodservService } from '../getmyprodserv.service';
 export class GetprodComponent implements OnInit {
 
   constructor(private mydata:GetmyprodservService) { }
+
+  veg:any=["tamoto","patato","brijal","Cocumber","onion"];
+  fruits:any=["Apple","banana","chico","Graps","Straberry"]
+  spices:any=[
+    {"name":'Tulsi ','price':100},
+    {"name":'Dalchini  ','price':260},
+    {"img":'path ','price':260}
+  ]
+
+  mykicknitem:any=[];
+
+  kichen1(e:any){
+    
+    
+    if(e=="veg"){
+        this.mykicknitem=this.veg;
+    }
+    if(e=="fruits"){
+      this.mykicknitem=this.fruits;
+  }
+  if(e=="spices"){
+    this.mykicknitem=this.spices;
+}
+
+  }
+  kichen(e:any){
+    
+    
+    if(e.target.value=="veg"){
+        this.mykicknitem=this.veg;
+    }
+    if(e.target.value=="fruits"){
+      this.mykicknitem=this.fruits;
+  }
+  if(e.target.value=="spices"){
+    this.mykicknitem=this.spices;
+}
+
+  }
+
 
   myproduct:any=[];
   ngOnInit(): void {
